51b7e103c641a00dda4fcf0ebfd01c851c421084
ritwikvulli7 <136991127+ritwikvulli7@users.noreply.github.com>
2024-12-27 11:06:45 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 3;
+    System.out.println(x == y); // returns false because 5 is not equal to 3
+  }
+}
+
